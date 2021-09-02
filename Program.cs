b0c8e84f39ab1eb3e4ec3be354f9d0fcf4297277
anilannangi <89434342+anilannangi@users.noreply.github.com>
2021-09-02 21:55:12 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SyncronizationExam5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
